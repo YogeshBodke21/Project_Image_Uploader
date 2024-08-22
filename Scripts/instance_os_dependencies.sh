299af54276cab526572ca583e9f74239c40954cc
Yogesh B <ybodke123@gmail.com>
2024-08-22 12:40:09 +0530

    updated

--- Scripts/instance_os_dependencies.sh
+++ Scripts/instance_os_dependencies.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
 sudo apt install -y python3-pip
+
 sudo apt install -y nginx
 sudo apt install -y virtualenv
+
+
+# Create a new virtual environment
+virtualenv venv
+source venv/bin/activate
+sudo pip3 install -r requirements.txt
